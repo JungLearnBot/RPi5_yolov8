2bf9c1553a7bcdd40b0c5185be2da5bf10031d60
jung <jung.learn.bot@gmail.com>
2023-12-17 17:49:58 +1300

    adding transfer weight as recommended from ultralytics. minor clean up

--- train.py
+++ train.py
@@ -10,12 +10,21 @@ if __name__ == '__main__':
     parser.add_argument("--imgsz", type=int, default=320)
     parser.add_argument("--batch", type=int, default=128)
     parser.add_argument("--save_period", type=int, default=50)
+
+    parser.add_argument("--data", default="coco.yaml", type=str,
+                        help="")
+    parser.add_argument("--transfer", default="yolov8n.pt", type=str,
+                        help="")
+
     args = parser.parse_args()
 
     model = YOLO(args.model)
 
+    if args.transfer is not None and len(args.transfer) > 0:
+        print(f'transfer weight from {args.transfer}')
+        model = model.load(args.transfer)
+
     # Display model information (optional)
     model.info()
-
-    results = model.train(data='coco.yaml', epochs=args.epochs, imgsz=args.imgsz, batch=args.batch,
+    results = model.train(data=args.data, epochs=args.epochs, imgsz=args.imgsz, batch=args.batch,
                           save_period=args.save_period)
